575f5cfa97522d6cae736319898e4dd6420f0e9f
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-05 23:34:25 +0200

    successfully fetched and displayed data in my nextjs table

--- components/TableDemo.tsx
+++ components/TableDemo.tsx
@@ -9,26 +9,30 @@ import {
     TableHeader,
     TableRow,
   } from "@/components/ui/table"
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
   
 
+
+ 
  
   export function TableDemo() {
 
-    
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/api/users'); 
-      const users = await response.json();        
-      console.log(users);                         
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    }
-  };
+    const [users, setUsers] = useState([]) 
 
-  fetchData(); 
-}, []);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('/api/users'); 
+          const users = await response.json();  
+          setUsers(users)      
+          console.log(users);                         
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      };
+    
+      fetchData(); 
+    }, []);
 
 
     return (
@@ -42,13 +46,13 @@ useEffect(() => {
           </TableRow>
         </TableHeader>
         <TableBody>
-        
-            <TableRow >
-              <TableCell className="font-medium"></TableCell>
-              <TableCell></TableCell>
-              <TableCell className="text-right"></TableCell>
+         {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell className="font-medium">{user.id}</TableCell>
+              <TableCell>{user.user_name}</TableCell>
+              <TableCell className="text-right">{user.email}</TableCell>
             </TableRow>
-         
+          ))}
         </TableBody>
         
       </Table>
