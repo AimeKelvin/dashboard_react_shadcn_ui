67c7310eb73266b793b3c47fe844ba475f955071
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-05 22:09:52 +0200

    debugging the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import { SettingsDialog } from "@/components/settings-dialog"
 
-export function Page() {
+const Page = () => {
   return (
     <div className="flex h-svh items-center justify-center">
       <SettingsDialog />
     </div>
   )
-}
+};
+
+export default Page;
